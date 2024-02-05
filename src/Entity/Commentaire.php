c2bf60c8235c96ff3ca6df7c3fce5a5b29028a10
Florian F <fleuret.florian@laposte.net>
2024-02-05 14:52:00 +0100

    step 1 = les Entity, CRUD, et d:m:m pour la BDD

---
+++ src/Entity/Commentaire.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommentaireRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CommentaireRepository::class)]
+class Commentaire
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $message = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $note = null;
+
+    #[ORM\ManyToOne(inversedBy: 'commentaires')]
+    private ?User $auteur = null;
+
+    #[ORM\ManyToOne(inversedBy: 'commentaires')]
+    private ?Article $articleComment = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): static
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getNote(): ?float
+    {
+        return $this->note;
+    }
+
+    public function setNote(?float $note): static
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    public function getAuteur(): ?User
+    {
+        return $this->auteur;
+    }
+
+    public function setAuteur(?User $auteur): static
+    {
+        $this->auteur = $auteur;
+
+        return $this;
+    }
+
+    public function getArticleComment(): ?Article
+    {
+        return $this->articleComment;
+    }
+
+    public function setArticleComment(?Article $articleComment): static
+    {
+        $this->articleComment = $articleComment;
+
+        return $this;
+    }
+}
